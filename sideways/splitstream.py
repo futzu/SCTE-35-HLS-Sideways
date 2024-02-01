a6b283da53e4748c79deff5538e9461d35b6d515
Adrian <52701496+futzu@users.noreply.github.com>
2024-02-01 18:12:47 -0500

    splicepoint

--- sideways/splitstream.py
+++ sideways/splitstream.py
@@ -20,23 +20,23 @@ class SplitStream(IFramer):
             head = head + sep
         return f"{head}{tail}"
 
-    def split_at(self,segment, pts, output_dir):
-        splice_point= None
-        seg =segment.rsplit('/')[-1]
-        a_name=f'a-{seg}'
-        b_name=f'b-{seg}'
-        a_media =self.mk_uri(output_dir,a_name)
-        b_media =self.mk_uri(output_dir,b_name)
-        with open(a_media,'wb') as a:
-            with open(b_media,'wb') as b:
+    def split_at(self, segment, pts, output_dir):
+        splice_point = None
+        seg = segment.rsplit("/")[-1]
+        a_name = f"a-{seg}"
+        b_name = f"b-{seg}"
+        a_media = self.mk_uri(output_dir, a_name)
+        b_media = self.mk_uri(output_dir, b_name)
+        with open(a_media, "wb") as a:
+            with open(b_media, "wb") as b:
                 outfile = a
                 with reader(segment) as video:
                     for pkt in iter(partial(video.read, 188), b""):
-                        iframe_pts=self.parse(pkt)
-                        if iframe_pts:
-                            if iframe_pts >= pts:
-                                if not splice_point:
-                                    splice_point= iframe_pts
-                                outfile = b
+                        if not splice_point:
+                            iframe_pts = self.parse(pkt)
+                            if iframe_pts:
+                                if iframe_pts >= pts:
+                                    splice_point = iframe_pts
+                                    outfile = b
                         outfile.write(pkt)
         return splice_point, a_media, b_media
